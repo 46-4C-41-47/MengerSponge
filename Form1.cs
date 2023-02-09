c72819b57ff0691551b70ab8b983032f3df11976
46-4C-41-47 <122835124+46-4C-41-47@users.noreply.github.com>
2023-02-09 14:20:45 +0100

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -4,36 +4,42 @@ namespace MengerSponge;
 
 public partial class Form1 : Form
 {
-    private readonly Point[] _attractors;
+    private static readonly Color BackgroundColor = Color.Black;
+    private static readonly Color CarpetColor = Color.White;
+    private const int Iterations = 200000;
+    private readonly int _xOffset, _yOffset;
+    private readonly Size _carpetSize = new (400, 400);
     private readonly Panel _canvas = new ();
+    private readonly Point[] _attractors;
     
     public Form1()
     {
         InitializeComponent();
         CenterToScreen();
-        FormBorderStyle = FormBorderStyle.FixedToolWindow;
-        Width = 400;
-        Height = 400;
+        FormBorderStyle = FormBorderStyle.FixedSingle;
+        Width = 600;
+        Height = 600;
         _attractors = InitAttractors();
+
+        _xOffset = (Width - _carpetSize.Width) / 2;
+        _yOffset = (Height - _carpetSize.Height) / 2;
         
         Controls.Add(_canvas);
-        _canvas.BackColor = Color.Black;
+        _canvas.BackColor = BackgroundColor;
         _canvas.Dock = DockStyle.Fill;
         
-        _canvas.Paint += (DrawAttractors);
+        _canvas.Paint += (DrawCarpet);
     }
 
 
-    private void DrawAttractors(object? sender, PaintEventArgs e)
+    private void DrawCarpet(object? sender, PaintEventArgs e)
     {
         var g = e.Graphics;
         var random = new Random();
-        var brush = new SolidBrush(Color.White);
-        var previousPoint = new Point(
-            (int) random.NextInt64() % Width, 
-            (int) random.NextInt64() % Height);
+        var brush = new SolidBrush(CarpetColor);
+        var previousPoint = new Point(0, 0);
 
-        for (var i = 0; i < 100000; i++)
+        for (var i = 0; i < Iterations; i++)
         {
             var attractor = _attractors[random.NextInt64() % _attractors.Length];
             
@@ -45,7 +51,12 @@ public partial class Form1 : Form
             var y = (int)(previousPoint.Y + vector.Y / 3 * 2);
             
             var nextPoint = new Point(x, y);
-            g.FillRectangle(brush, nextPoint.X, nextPoint.Y, 1, 1);
+            g.FillRectangle(
+                brush, 
+                nextPoint.X + _xOffset,
+                nextPoint.Y + _yOffset,
+                1, 1);
+            
             previousPoint = nextPoint;
         }
     }
@@ -53,7 +64,7 @@ public partial class Form1 : Form
 
     private Point[] InitAttractors()
     {
-        var squareWidth = Width < Height ? Width : Height;
+        var squareWidth = _carpetSize.Width;
         
         Point[] points =
         {
